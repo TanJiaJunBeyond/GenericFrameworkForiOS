d225071acf5277919f695fe4310fec375285de0c
TanJiaJun <1120571286@qq.com>
2019-11-16 20:44:01 +0800

    新建项目。

---
+++ GenericFramworkForiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericFramworkForiOS
+//
+//  Created by 谭嘉俊 on 2019/11/16.
+//  Copyright © 2019 谭嘉俊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GenericFramworkForiOSTests/GenericFramworkForiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  GenericFramworkForiOSTests.swift
+//  GenericFramworkForiOSTests
+//
+//  Created by 谭嘉俊 on 2019/11/16.
+//  Copyright © 2019 谭嘉俊. All rights reserved.
+//
+
+import XCTest
+@testable import GenericFramworkForiOS
+
+class GenericFramworkForiOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
